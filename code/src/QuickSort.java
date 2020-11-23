cdeb7c69e608338036d1b666b068ed637812c6ef
ArtGeekTech <agt@ArtGeekTechs-Mac-mini.local>
2020-11-23 12:07:14 -0800

    qsort

---
+++ code/src/QuickSort.java
@@ -0,0 +1,37 @@
+public class QuickSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        qsort(arr, 0, arr.length - 1);
+    }
+
+    private static void qsort(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int p = partition(arr, start, end);
+        qsort(arr, start, p - 1);
+        qsort(arr, p + 1, end);
+    }
+
+    // choose arr[end] as pivot around which to partition the subarray
+    // [start, p] (<pivot) + [p+1, i-1] (>=pivot) + [i, end-1] (todo)
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[end], p = start - 1;
+        for (int i = start; i <= end - 1; i++) {
+            if (arr[i] < pivot) {
+                swap(arr, ++p, i);
+            }
+        }
+        swap(arr, ++p, end);
+        return p;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
