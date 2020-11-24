9efa78e10372c874bdc719183f1f905847bb64e9
ArtGeekTech <agt@ArtGeekTechs-Mac-mini.local>
2020-11-24 11:49:25 -0800

    2

---
+++ code/src/FirstLastPositionSortedArray.java
@@ -0,0 +1,2 @@
+public class FirstLastPositionSortedArray {
+}
---
+++ code/src/FirstPositionSortedArray.java
@@ -0,0 +1,26 @@
+public class FirstPositionSortedArray {
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0, end = nums.length - 1, mid = 0;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid;
+            } else if (nums[mid] > target) {
+                end = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+}
