d2666eb4d1eb097ea640aca14e4109fea0e4eac6
ArtGeekTech <agt@ArtGeekTechs-Mac-mini.local>
2020-11-29 23:30:03 -0800

    21

---
+++ code/src/SearchRotatedSortedArrayII.java
@@ -0,0 +1,32 @@
+public class SearchRotatedSortedArrayII {
+    public static boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[start] < nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else if (nums[start] > nums[mid]) {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            } else {
+                start++;
+            }
+        }
+
+        return nums[start] == target || nums[end] == target;
+    }
+}
