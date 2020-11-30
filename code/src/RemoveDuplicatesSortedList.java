181fbe3ea2e5a0ec76fad46ae4c9c1e88a235a03
ArtGeekTech <agt@ArtGeekTechs-Mac-mini.local>
2020-11-30 14:29:33 -0800

    28

---
+++ code/src/RemoveDuplicatesSortedList.java
@@ -0,0 +1,17 @@
+public class RemoveDuplicatesSortedList {
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
