aa6255617b8e36b9f1bb225c2050bc37ae367174
ArtGeekTech <agt@ArtGeekTechs-Mac-mini.local>
2021-01-06 18:31:12 -0800

    45

---
+++ code/src/BinaryTreePostorderTraversal.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    private void postorder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        postorder(root.left, list);
+        postorder(root.right, list);
+        list.add(root.val);
+    }
+}
